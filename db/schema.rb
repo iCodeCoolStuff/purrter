dd3a0c3fcf0b18effa81264097e66e6f7f2bcba9
iCodeCoolStuff <cakelord@protonmail.com>
2019-08-08 16:37:24 -0400

    add admin user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_06_011736) do
+ActiveRecord::Schema.define(version: 2019_08_08_202645) do
+
+  create_table "purrs", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_purrs_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,6 +30,8 @@ ActiveRecord::Schema.define(version: 2019_08_06_011736) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.string "username"
+    t.integer "role"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
