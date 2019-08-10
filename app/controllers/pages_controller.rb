8edff0d782eefa8c07442f3246399752a1e9f94d
iCodeCoolStuff <cakelord@protonmail.com>
2019-08-10 12:36:05 -0400

    temp commit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,8 @@
 class PagesController < ApplicationController
 
   def home
+    @user = current_user
+    @purrs = @user.purrs.to_a
   end
 
   def index
--- db/schema.rb
+++ db/schema.rb
@@ -30,7 +30,6 @@ ActiveRecord::Schema.define(version: 2019_08_08_202645) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.string "username"
-    t.integer "role"
     t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
