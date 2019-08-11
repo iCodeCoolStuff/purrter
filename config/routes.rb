61648c423b6f03b48a9627cec250c3e398ea17d7
iCodeCoolStuff <cakelord@protonmail.com>
2019-08-10 20:18:07 -0400

    temp commit 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations'}
   resources :purrs
-  get '/', to: 'pages#home'
+  get '/home', to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  #root to: "home#index"
+  #root to: "/home"
 end
