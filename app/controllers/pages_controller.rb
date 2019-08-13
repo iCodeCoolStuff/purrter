ca0e12bdfd6374539fd144bb80c486c594938efe
iCodeCoolStuff <cakelord@protonmail.com>
2019-08-12 23:19:20 -0400

    add purr to home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,6 @@ class PagesController < ApplicationController
 
   def home
     @user = current_user
-    @purrs = @user.purrs.to_a
   end
 
   def index
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def user_avatar(u)
+    if u.avatar.attached?
+      image_tag u.avatar
+    else
+      image_tag 'default-profile-pic.png', style: "width: 100%;"
+    end
+  end
 end
