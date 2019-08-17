a57cc70720ded6d4ddf0c810c37d9a61bfb94b7f
iCodeCoolStuff <cakelord@protonmail.com>
2019-08-17 12:57:49 -0400

    fix merge conflict

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations'}
+=begin
   resources :users do
     member do
       get :following, :followers
     end
   end
+=end
+  #resources :users, only: [:show]
   resources :purrs
+  resources :relationships, only: [:create, :destroy]
   get '/home', to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #root to: "/home"
